3bf246dc268be8103117bd3f954c84bca5b7b6c1
Alexander Alemayhu <alexander@alemayhu.com>
2018-10-13 01:09:46 +0200

    Setting up skeleton for game
    Get the snake moving around the screen with "simulated" actions #TweetThatCommit

--- src/prefabs/snake.ts
+++ src/prefabs/snake.ts
@@ -1,16 +1,55 @@
 import Phaser from 'phaser-ce';
 
 export class Snake extends Phaser.Sprite {
-    constructor(game: Phaser.Game, x: number, y: number) {
-        super(game, x, y, 'snake');
+  private direction: string;
+  private id: string;
 
-        this.anchor.setTo(0.5);
-        this.game.physics.arcade.enableBody(this);
-        this.checkWorldBounds = true;
-        this.body.collideWorldBounds = true;
-    }
+  constructor(id: string, game: Phaser.Game, x: number, y: number) {
+    super(game, x, y, 'snake');
 
-    update() {
-        this.angle += 1;
+    this.id = id;
+    this.anchor.setTo(0.5);
+    this.game.physics.arcade.enableBody(this);
+    this.checkWorldBounds = true;
+    this.body.collideWorldBounds = true;
+  }
+
+  update() {
+    switch (this.direction) {
+      case 'right': {
+        this.position.x++;
+        break;
+      }
+      case 'left': {
+        this.position.x--;
+        break;
+      }
+      case 'up': {
+        this.position.y--;
+        break;
+      }
+      case 'down': {
+        this.position.y++;
+        break;
+      }
     }
+  }
+
+  run(action) {
+    switch (action) {
+      case 'attack': {
+        // TODO: Attack
+        break;
+      }
+      case 'collect': {
+        // TODO: Collect
+        break;
+      }
+      // Move
+      default: {
+      this.direction = action;
+      break;
+    }
+  }
+}
 }
--- src/states/game.ts
+++ src/states/game.ts
@@ -6,44 +6,46 @@ import {Sound} from '../helpers/sound';
 import {Snake} from '../prefabs/Snake';
 
 export class Game extends Phaser.State {
-    private Snake: Phaser.Sprite;
-    private cursors: Phaser.CursorKeys;
-    private text: Phaser.BitmapText;
-    private spaceKey: Phaser.Key;
+  private Snake: Snake;
+  // private cursors: Phaser.CursorKeys;
+  private text: Phaser.BitmapText;
+  private spaceKey: Phaser.Key;
+  private tick: number;
+  private loopTick = 1000;
+  private actions = ['attack', 'heal', 'collect', 'right', 'left', 'up', 'down'];
 
-    public create(): void {
-        this.game.physics.startSystem(Phaser.Physics.ARCADE);
+  public create(): void {
+    this.game.physics.startSystem(Phaser.Physics.ARCADE);
 
-        this.text = this.game.add.bitmapText(this.game.world.centerX, this.game.world.centerY + 100, 'font', 'Press Arrows / Space', 15);
-        this.text.x = this.text.x - ~~(this.text.width * 0.5);
+    this.text = this.game.add.bitmapText(this.game.world.centerX, this.game.world.centerY + 100, 'font', 'Press Arrows / Space', 15);
+    this.text.x = this.text.x - ~~(this.text.width * 0.5);
 
-        this.Snake = new Snake(this.game, this.game.world.centerX, this.game.world.centerY);
-        this.game.add.existing(this.Snake);
+    // TODO: Add more snakes
+    this.Snake = new Snake("snake-1", this.game, this.game.world.centerX, this.game.world.centerY);
+    this.game.add.existing(this.Snake);
 
-        this.cursors = this.game.input.keyboard.createCursorKeys();
+    // this.cursors = this.game.input.keyboard.createCursorKeys();
 
-        this.spaceKey = this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-        this.spaceKey.onDown.add(() => {
-            Sound.play();
-            this.Snake.x = this.game.world.centerX;
-            this.Snake.y = this.game.world.centerY;
-        }, this);
-    }
+    this.spaceKey = this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    this.spaceKey.onDown.add(() => {
+      Sound.play();
+      this.Snake.x = this.game.world.centerX;
+      this.Snake.y = this.game.world.centerY;
+    }, this);
+    this.tick = this.game.time.now;
+  }
 
-    public update(): void {
-        this.game.input.update();
+  public update(): void {
+    this.game.input.update();
+    let tock = this.game.time.now - this.tick;
+    // Limit the run loop to every x
+    if (tock < this.loopTick) { return; }
+    this.tick = this.game.time.now;
 
-        if (this.cursors.down.isDown) {
-            this.Snake.position.y++;
-        }
-        if (this.cursors.up.isDown) {
-            this.Snake.position.y--;
-        }
-        if (this.cursors.left.isDown) {
-            this.Snake.position.x--;
-        }
-        if (this.cursors.right.isDown) {
-            this.Snake.position.x++;
-        }
-    }
+    // TODO: run actions on all players
+    let index = Math.floor((Math.random() * this.actions.length) | 0);
+    let action = this.actions[index];
+    this.text.setText(action);
+    this.Snake.run(action);
+  }
 }
