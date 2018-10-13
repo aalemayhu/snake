2abe4bb872fb9a387b64d66cb5867c0edf71fdfc
Nyasaki <walk.marcel.97@googlemail.com>
2018-10-13 21:09:29 +0200

    Added validation to the !upload command Added file-saver to the package.json

--- src/scraping/scraper.ts
+++ src/scraping/scraper.ts
@@ -2,21 +2,33 @@ import rp from 'request-promise';
 import cheerio from 'cheerio';
 import request from 'request';
 import fs from 'fs';
+import filesaver from 'file-saver';
 
 export class Scraper {
   constructor() {}
 
   GetPageContent(url, username, callback): boolean {
     let requestResult = false;
-    let req = request(url, function (error, response, body) {
-      console.log('Errors: ' + '\n' + 'Response: ' + response.statusCode + '\n');
-      if (error === null && response.statusCode === 200) {
-        requestResult = true;
-        callback(requestResult);
-        console.log(requestResult);
-      }
-    });
-    req.pipe(fs.createWriteStream('./' + username + '.js'));
+    let filePath = url + '';
+
+      let req = request(url, function (error, response, body) {
+        console.log('Errors: ' + '\n' + 'Response: ' + response.statusCode + '\n');
+
+        if (error === null && response.statusCode === 200) {
+          requestResult = true;
+
+          console.log(username);
+
+          //TODO Wait for electron so its saved locally
+          let filename = username + '.ts'
+          var blob = new Blob([body], {type: "text/plain;charset=utf-8"});
+          filesaver.saveAs(blob, filename);
+
+          callback(requestResult);
+        }
+
+      });
+      
     console.log(requestResult);
     return requestResult;
   }
--- src/twitch/TwitchChat.ts
+++ src/twitch/TwitchChat.ts
@@ -13,13 +13,17 @@ export class TwitchChat {
       if (message.content.startsWith('!upload')) {
         let linkToSource = message.content.replace('!upload' , '');
         if (linkToSource.indexOf('gist') !== -1) {
-          scraper.GetPageContent('https://' + linkToSource.trim(), message.displayName, (result) => {
-            if (result) {
-              twitch.send('@' + message.displayName + ' file checked and loaded', message.channel);
-            } else {
-              twitch.send('@' + message.displayName + ' could not download file', message.channel);
-            }
-          });
+          if(linkToSource.endsWith('.txt') || linkToSource.endsWith('.ts')){
+            scraper.GetPageContent('https://' + linkToSource.trim(), message.displayName.toString() , (result) => {
+              if (result) {
+                twitch.send('@' + message.displayName + ' file checked and loaded', message.channel);
+              } else {
+                twitch.send('@' + message.displayName + ' could not download file', message.channel);
+              }
+            });
+          } else {
+            twitch.send('@' + message.displayName + ' please use the gist raw link', message.channel);
+          }
         } else {
           twitch.send('@' + message.displayName + ' please use Gist to upload your file', message.channel);
         }
