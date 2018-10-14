e2e3b34b021e797788ec6d874a54f98def83601a
Alexander Alemayhu <alexander@alemayhu.com>
2018-10-14 18:03:50 +0200

    Use graphics to draw the snake
    Still have to fix the bounds collision with world.

--- src/prefabs/snake.ts
+++ src/prefabs/snake.ts
@@ -1,11 +1,12 @@
 import Phaser from 'phaser-ce';
 
-export class Snake extends Phaser.Sprite {
+export class Snake extends Phaser.Graphics {
   readonly id: string;
+  // TODO rename movementUnits to cellSize
   private movementUnits: number;
 
   constructor(id: string, game: Phaser.Game, x: number, y: number, cellSize: number) {
-    super(game, x, y, 'snake');
+    super(game, x, y);
 
     this.id = id;
     this.game.physics.arcade.enableBody(this);
@@ -16,6 +17,9 @@ export class Snake extends Phaser.Sprite {
   }
 
   update() {
+    this.clear();
+    this.beginFill(0xFF0000, 1);
+    this.drawRect(this.position.x, this.position.y, this.movementUnits, this.movementUnits);
   }
 
   move(direction) {
