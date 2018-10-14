d6081dfc004a309976a73fdd5ccd5037b9851e02
Nyasaki <walk.marcel.97@googlemail.com>
2018-10-14 20:06:08 +0200

    Changed the ApiHandler

--- src/api/ApiHandler.ts
+++ src/api/ApiHandler.ts
@@ -1,20 +1,27 @@
 import { SnakeApi } from './SnakeApi';
+import { Snake } from '../prefabs/Snake';
 
-let scripts: SnakeApi.Snake [];
-scripts = [];
+let scripts: string [] = new Array(1000);
 
 export class ApiHandler {
     constructor() {}
 
     LoadAllScripts() {
         // Get files in directory
-
-        scripts.push(); // add file to array
+        scripts.push('../Scripts/Example.ts'); // add file to array
     }
 
     RunAllScripts() {
         scripts.forEach(element => {
-            element.Run();
+            if (element !== undefined) {
+                this.RunScript(element);
+            }
+        });
+    }
+
+    RunScript(path: string) {
+        import(path).then(script => {
+            script.Run();
         });
     }
 }
