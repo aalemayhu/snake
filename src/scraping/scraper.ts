38cca7c008f87e9e3b8d1bc1af9f286f3e3f2313
Mads Cordes <Mobilpadde@gmail.com>
2018-10-14 22:43:00 +0200

    Tests are stupid! Especially in ts

--- src/scraping/scraper.ts
+++ src/scraping/scraper.ts
@@ -5,33 +5,53 @@ import fs from 'fs';
 import filesaver from 'file-saver';
 
 export class Scraper {
-  constructor() {}
+    constructor() {}
 
-  // Method to get the user script from gist
-  GetPageContent(url, username, callback): boolean {
-    let requestResult = false;
-    let filePath = url + '';
+    static GetRawUrl(url: string): Promise<string> {
+        return new Promise((resolve, reject) => {
+            if (url.indexOf('raw') > -1) {
+                resolve(url);
+            }
 
-      // Request the userscript
-      let req = request(url, function (error, response, body) {
-        console.log('Errors: ' + '\n' + 'Response: ' + response.statusCode + '\n');
+            request(url, (err, res, body) => {
+                if (err) {
+                    reject(err);
+                }
 
-        // Checks if there was an error requesting
-        if (error === null && response.statusCode === 200) {
-          requestResult = true;
+                resolve('https://gist.github.com/' + cheerio.load(body)('.file-actions .btn').href());
+            });
+        });
+    }
 
-          // TODO Wait for electron so its saved locally
-          // Save the content of the userscript to a new file (username.ts)
-          let filename = username + '.ts';
-          let blob = new Blob([body], {type: 'text/plain;charset=utf-8'});
-          filesaver.saveAs(blob, filename);
+    // Method to get the user script from gist
+    GetPageContent(url, username, callback): boolean {
+        const filePath = url + '';
+        let requestResult = false;
 
-          // Returns the result of the request
-          callback(requestResult);
+        if (url.indexOf('raw') === -1) {
+            url = Scraper.GetRawUrl(url);
         }
 
-      });
-    console.log(requestResult);
-    return requestResult;
-  }
+        // Request the userscript
+        const req = request(url, function (error, response, body) {
+            console.log('Errors: ' + '\n' + 'Response: ' + response.statusCode + '\n');
+
+            // Checks if there was an error requesting
+            if (error === null && response.statusCode === 200) {
+                requestResult = true;
+
+                // TODO Wait for electron so its saved locally
+                // Save the content of the userscript to a new file (username.ts)
+                const filename = username + '.ts';
+                const blob = new Blob([body], {type: 'text/plain;charset=utf-8'});
+                filesaver.saveAs(blob, filename);
+
+                // Returns the result of the request
+                callback(requestResult);
+            }
+        });
+        console.log(requestResult);
+
+        return requestResult;
+    }
 }
