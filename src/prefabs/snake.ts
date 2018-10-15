890c159c628a9667c86cf1f5b4620c525e5104eb
Alexander Alemayhu <alexander@alemayhu.com>
2018-10-15 18:50:23 +0200

    Fix the player position by divding the draw position and cell
    Thanks to fnipfbojdblk for suggestions.

--- src/prefabs/snake.ts
+++ src/prefabs/snake.ts
@@ -2,89 +2,92 @@ import Phaser from 'phaser-ce';
 
 export class Snake extends Phaser.Graphics {
   readonly id: string;
-  private movementUnits: number;
-  private color: number;
-  private worldHeight: number;
-  private worldWidth: number;
+  private cellSize: number;
+  public color: number;
+
+  private positions: number[][];
 
   constructor(id: string, game: Phaser.Game, x: number, y: number, cellSize: number) {
     super(game, x, y);
 
-    this.worldWidth = this.game.width;
-    this.worldHeight = this.game.height;
+    this.positions = [];
+    this.width = this.game.width;
+    this.height = this.game.height;
 
     this.id = id;
-    this.movementUnits = cellSize;
+    this.cellSize = cellSize;
     this.color = 0xFF0000;
-    this.position.x = x;
-    this.position.y = y;
+    this.positions.push([x, y])
   }
 
   update() {
   }
 
-  draw() {
-    this.lineStyle(1, 0x0000FF, 1);
-    this.beginFill(0xd88a8a);
-    this.drawRect(
-      this.position.x,
-      this.position.y,
-      this.movementUnits,
-      this.movementUnits
+  draw(graphics) {
+    graphics.lineStyle(1, 0x0000FF, 1);
+    graphics.beginFill(this.color);
+    //   for (let i = 0; i < this.positions.length; i++) {
+    //
+    //   }
+
+    graphics.drawRect(
+      this.position.x * this.cellSize,
+      this.position.y * this.cellSize, this.cellSize,
+      this.cellSize
     );
+      graphics.endFill()
+      console.log(`${this.id}.drawRect(${this.position}, ${this.cellSize})`);
+    }
 
-    console.log(`${this.id}.drawRect(${this.position}, ${this.movementUnits})`);
-  }
-
-  move(direction) {
-    console.log(`move(${direction})`);
-    switch (direction) {
-      case 'right': {
-        if (this.position.x + this.movementUnits >= this.worldWidth) {
-        return;
+    move(direction) {
+      console.log(`move(${direction})`);
+      switch (direction) {
+        case 'right': {
+          if (this.position.x + this.cellSize >= this.height) {
+            return;
+          }
+          this.position.x += this.cellSize;
+          break;
         }
-        this.position.x += this.movementUnits;
-        break;
-      }
-      case 'left': {
-        if (this.position.x - this.movementUnits <= this.movementUnits) {
+        case 'left': {
+          if (this.position.x - this.cellSize <= this.cellSize) {
             return;
           }
-        this.position.x -= this.movementUnits;
-        break;
-      }
-      case 'up': {
-        if (this.position.y + this.movementUnits >= this.worldHeight) {
+          this.position.x -= this.cellSize;
+          break;
+        }
+        case 'up': {
+          if (this.position.y + this.cellSize >= this.height) {
             return;
           }
-        this.position.y += this.movementUnits;
-        break;
-      }
-      case 'down': {
-        if (this.position.y - this.movementUnits <= this.movementUnits) {
+          this.position.y += this.cellSize;
+          break;
+        }
+        case 'down': {
+          if (this.position.y - this.cellSize <= this.cellSize) {
             return;
           }
-        this.position.y -= this.movementUnits;
-        break;
+          this.position.y -= this.cellSize;
+          break;
+        }
       }
     }
-  }
 
-  run(action) {
-    switch (action) {
-      case 'attack': {
-        // TODO: Attack
+    run(action) {
+      switch (action) {
+        case 'attack': {
+          // TODO: Attack
+          break;
+        }
+        case 'collect': {
+          // TODO: Collect
+          break;
+        }
+        // Move
+        default: {
+        this.move(action);
         break;
       }
-      case 'collect': {
-        // TODO: Collect
-        break;
-      }
-      // Move
-      default: {
-      this.move(action);
-      break;
     }
   }
-}
 }
--- src/states/game.ts
+++ src/states/game.ts
@@ -7,7 +7,6 @@ import {Snake} from '../prefabs/Snake';
 
 export class Game extends Phaser.State {
   private players: Snake[];
-  private grid: Phaser.Line[];
   private Snake: Snake;
   // private cursors: Phaser.CursorKeys;
   private spaceKey: Phaser.Key;
@@ -16,12 +15,19 @@ export class Game extends Phaser.State {
   // 'attack', 'heal', 'collect',
   private actions = ['right', 'left', 'up', 'down'];
 
+  private grid: Phaser.Graphics;
+
   private cellSize = 32;
+  private cellX: number;
+  private cellY: number;
 
   public create(): void {
     this.game.physics.startSystem(Phaser.Physics.ARCADE);
     this.createGrid();
-    this.game.load.image('snake', 'assets/sprites/snake.png');
+    this.grid = this.game.add.graphics(0, 0);
+
+    this.cellX = this.game.width / this.cellSize;
+    this.cellY = this.game.height / this.cellSize;
 
     // TODO: movement as grid units
     // TODO: figure out how big a snake is supposed be, for now make it tuneable
@@ -31,73 +37,72 @@ export class Game extends Phaser.State {
       let snake = this.newSnake(`snake-${i}`);
       this.players.push(snake);
       this.game.add.existing(snake);
-      snake.draw()
-    }
 
-    // this.cursors = this.game.input.keyboard.createCursorKeys();
+    // Add debugging colors
+    if (i === 0) { snake.color = Phaser.Color.RED; }
+    if (i === 1) { snake.color = Phaser.Color.GREEN; }
+    if (i === 2) { snake.color = Phaser.Color.BLUE; }
 
-    this.spaceKey = this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-    this.spaceKey.onDown.add(() => {
-      Sound.play();
-      this.Snake.x = this.game.world.centerX;
-      this.Snake.y = this.game.world.centerY;
-    }, this);
-    this.tick = this.game.time.now;
+    snake.draw(this.grid)
   }
 
-  getRandomInt(max): number {
-    return Math.floor(Math.random() * Math.floor(max));
+  // this.cursors = this.game.input.keyboard.createCursorKeys();
+
+  this.spaceKey = this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+  this.spaceKey.onDown.add(() => {
+    Sound.play();
+  }, this);
+  this.tick = this.game.time.now;
+}
+
+getRandomInt(max): number {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+newSnake(id: string): Snake {
+  let x = this.getRandomInt(this.cellX);
+  let y = this.getRandomInt(this.cellY);
+  // TODO: check if spawn point is already taken by another user
+  let s = new Snake(id, this.game, x, y, this.cellSize);
+  return s;
+}
+
+public update(): void {
+  this.game.input.update();
+  let tock = this.game.time.now - this.tick;
+  // Limit the run loop to every x
+  if (tock < this.loopTick) { return; }
+  this.tick = this.game.time.now;
+
+  for (let i = 0; i < this.players.length; i++) {
+    let snake = this.players[i];
+    let index = Math.floor((Math.random() * this.actions.length) | 0);
+    let action = this.actions[index];
+    // snake.run(action);
   }
+}
 
-  newSnake(id: string): Snake {
-    let cellX = this.getRandomInt(this.game.width / this.cellSize);
-    let cellY = this.getRandomInt(this.game.height / this.cellSize);
-    let x = cellX * this.cellSize;
-    let y = cellY * this.cellSize;
-    console.log(`cellX=${cellX},cellY=${cellY}`);
-    // TODO: check if spawn point is already taken by another user
-    let s = new Snake(id, this.game, x, y, this.cellSize);
-    s.tint = Phaser.Color.WHITE;
-    console.log(`${s.id}.position=${s.position}`);
-    return s;
-  }
+createGrid() {
+  let graphics = this.game.add.graphics(0, 0);
+  let style = { font: '8px Arial', fill: '#ff0044', wordWrap: true,
+  wordWrapWidth: this.cellSize, align: 'center', backgroundColor: '#ffff00' };
+  // set a fill and line style
+  graphics.beginFill(0xFF3300);
+  graphics.lineStyle(10, 0xffd900, 1);
 
-  public update(): void {
-    this.game.input.update();
-    let tock = this.game.time.now - this.tick;
-    // Limit the run loop to every x
-    if (tock < this.loopTick) { return; }
-    this.tick = this.game.time.now;
+  // set a fill and line style again
+  graphics.lineStyle(10, 0xFF0000, 0.8);
+  graphics.beginFill(0xFF700B, 1);
 
-    for (let i = 0; i < this.players.length; i++) {
-      let snake = this.players[i];
-      let index = Math.floor((Math.random() * this.actions.length) | 0);
-      let action = this.actions[index];
-      // snake.run(action);
-    }
-  }
+  // draw a rectangle
+  graphics.lineStyle(2, 0x0000FF, 1);
 
-  createGrid() {
-    let graphics = this.game.add.graphics(0, 0);
-    let style = { font: '8px Arial', fill: '#ff0044', wordWrap: true,
-    wordWrapWidth: this.cellSize, align: 'center', backgroundColor: '#ffff00' };
-    // set a fill and line style
-    graphics.beginFill(0xFF3300);
-    graphics.lineStyle(10, 0xffd900, 1);
-
-    // set a fill and line style again
-    graphics.lineStyle(10, 0xFF0000, 0.8);
-    graphics.beginFill(0xFF700B, 1);
-
-    // draw a rectangle
-    graphics.lineStyle(2, 0x0000FF, 1);
-
-    for (let i = 0; i < this.game.width / this.cellSize; i++) {
-      for (let j = 0; j < this.game.height / this.cellSize; j++) {
-        graphics.drawRect(i * this.cellSize, j * this.cellSize, this.cellSize, this.cellSize);
-        // let t = `[${i * this.cellSize}x${j * this.cellSize}]`
-        // this.game.add.text(i * this.cellSize, j * this.cellSize, t, style);
-      }
+  for (let i = 0; i < this.game.width / this.cellSize; i++) {
+    for (let j = 0; j < this.game.height / this.cellSize; j++) {
+      // graphics.drawRect(i * this.cellSize, j * this.cellSize, this.cellSize, this.cellSize);
+      // let t = `[${i * this.cellSize}x${j * this.cellSize}]`
+      // this.game.add.text(i * this.cellSize, j * this.cellSize, t, style);
     }
   }
+}
 }
