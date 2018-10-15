75e0f7ed1beac3131a1e590c58ef27a899821e63
Alexander Alemayhu <alexander@alemayhu.com>
2018-10-15 18:00:53 +0200

    Remove merge conflict residue

--- src/app.ts
+++ src/app.ts
@@ -7,10 +7,7 @@ import {TwitchChat} from './twitch/TwitchChat';
 import {Boot} from './states/boot';
 import {Preload} from './states/preload';
 import {Game} from './states/game';
-<<<<<<< HEAD
 import { ApiHandler } from './api/ApiHandler';
-=======
->>>>>>> 8a06a6a4f46713a787fbe554db308eb43c4c5b96
 
 class Template extends Phaser.Game {
 
@@ -23,19 +20,12 @@ class Template extends Phaser.Game {
 
         this.state.start('Boot');
         let twitch = new TwitchChat ('nyasaki_bot', 'ccscanf' , 'byqcq486mbcznovxrjmh6z3y25hign');
-<<<<<<< HEAD
         let h = new ApiHandler();
         h.LoadAllScripts();
         h.RunAllScripts();
-=======
->>>>>>> 8a06a6a4f46713a787fbe554db308eb43c4c5b96
     }
 }
 
 window.onload = () => {
     new Template();
-<<<<<<< HEAD
 };
-=======
-};
->>>>>>> 8a06a6a4f46713a787fbe554db308eb43c4c5b96
