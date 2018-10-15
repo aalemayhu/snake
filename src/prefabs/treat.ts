b6e3733f81cfb27d4241ed63f21518de53a46c2c
Alexander Alemayhu <alexander@alemayhu.com>
2018-10-15 22:32:20 +0200

    Move treats into a seperate class

---
+++ src/prefabs/treat.ts
@@ -0,0 +1,29 @@
+import Phaser from 'phaser-ce';
+
+export class Treat extends Phaser.Graphics {
+  private cellSize: number;
+  public color: number;
+
+  private cellX: number;
+  private cellY: number;
+
+  constructor(color: number, game: Phaser.Game, x: number, y: number, cellSize: number) {
+    super(game, x, y);
+    this.cellSize = cellSize;
+    this.color = color;
+    this.cellX = this.game.width / this.cellSize;
+    this.cellY = this.game.height / this.cellSize;
+  }
+
+  draw(graphics) {
+    graphics.lineStyle(1, 0x0000FF, 1);
+    graphics.beginFill(this.color);
+    graphics.drawRect(
+      this.position.x * this.cellSize,
+      this.position.y * this.cellSize,
+      this.cellSize,
+      this.cellSize
+    );
+    graphics.endFill()
+  }
+}
--- src/states/game.ts
+++ src/states/game.ts
@@ -4,6 +4,7 @@ import Phaser from 'phaser-ce';
 
 import {Sound} from '../helpers/sound';
 import {Snake} from '../prefabs/Snake';
+import {Treat} from '../prefabs/Treat';
 import { ApiHandler } from '../api/ApiHandler';
 
 export class Game extends Phaser.State {
@@ -22,7 +23,7 @@ export class Game extends Phaser.State {
   private cellSize = 32;
   private cellX: number;
   private cellY: number;
-  private treats: number[][];
+  private treats: Treat[];
   private expectedTreatCount = 32;
 
   public create(): void {
@@ -56,8 +57,6 @@ export class Game extends Phaser.State {
       snake.draw(this.grid)
     }
 
-    // this.cursors = this.game.input.keyboard.createCursorKeys();
-
     this.spaceKey = this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
     this.spaceKey.onDown.add(() => {
       Sound.play();
@@ -74,77 +73,72 @@ export class Game extends Phaser.State {
     }
 
     // Draw the treats
-    if (this.treats.length == 0) { return true };
     for (let i = 0; i < this.treats.length; i++) {
-    let treat = this.treats[i];
-    if (treat[0] == x && treat[1] == y) {
-      return false;
+      let treat = this.treats[i];
+      if (treat.position.x == x && treat.position.y == y) {
+        return false;
+      }
     }
-  }
 
-  return true;
-}
+    return true;
+  }
 
   getRandomInt(max): number {
     return Math.floor(Math.random() * Math.floor(max));
   }
 
-newTreat(): [number] {
-  // TODO: check if spawn point is already taken
-  let x = this.getRandomInt(this.cellX);
-  let y = this.getRandomInt(this.cellY);
-  while (!this.isCellAvailable(x, y)) {
-    x = this.getRandomInt(this.cellX);
-    y = this.getRandomInt(this.cellY);
-    // TODO: give up after trying x times
+  newTreat(): Treat {
+    // TODO: check if spawn point is already taken
+    let x = this.getRandomInt(this.cellX);
+    let y = this.getRandomInt(this.cellY);
+    while (!this.isCellAvailable(x, y)) {
+      x = this.getRandomInt(this.cellX);
+      y = this.getRandomInt(this.cellY);
+      // TODO: give up after trying x times
+    }
+    return new Treat(Phaser.Color.GRAY, this.game, x, y, this.cellSize)
   }
-  return [x,y]
-}
 
-newSnake(id: string): Snake {
-  let x = this.getRandomInt(this.cellX);
-  let y = this.getRandomInt(this.cellY);
-  // TODO: check if spawn point is already taken by another user
-  let s = new Snake(id, this.game, x, y, this.cellSize);
-  return s;
-}
+  newSnake(id: string): Snake {
+    let x = this.getRandomInt(this.cellX);
+    let y = this.getRandomInt(this.cellY);
+    // TODO: check if spawn point is already taken by another user
+    let s = new Snake(id, this.game, x, y, this.cellSize);
+    return s;
+  }
 
-public update(): void {
-  this.game.input.update();
-  let tock = this.game.time.now - this.tick;
-  // Limit the run loop to every x
-  if (tock < this.loopTick) { return; }
-  this.tick = this.game.time.now;
+  public update(): void {
+    this.game.input.update();
+    let tock = this.game.time.now - this.tick;
+    // Limit the run loop to every x
+    if (tock < this.loopTick) { return; }
+    this.tick = this.game.time.now;
 
-  this.grid.clear();
-  // Draw the players
-  for (let i = 0; i < this.players.length; i++) {
-  let snake = this.players[i];
-  let index = Math.floor((Math.random() * this.actions.length) | 0);
-  let action = this.actions[index];
-  snake.run(action);
-  snake.draw(this.grid)
-}
+    this.grid.clear();
+    // Draw the players
+    for (let i = 0; i < this.players.length; i++) {
+      let snake = this.players[i];
+      let index = Math.floor((Math.random() * this.actions.length) | 0);
+      let action = this.actions[index];
+      snake.run(action);
+      snake.draw(this.grid);
+    }
 
-// Make sure we have enough treats on screen
-for (;this.treats.length < this.expectedTreatCount;) {
-this.treats.push(this.newTreat())
-}
-// Draw the treats
-for (let i = 0; i < this.treats.length; i++) {
-let treat = this.treats[i];
-this.grid.lineStyle(1, 0x0000FF, 1);
-this.grid.beginFill(Phaser.Color.GRAY);
-this.grid.drawRect(treat[0] * this.cellX,
-  treat[1] * this.cellY, this.cellSize, this.cellSize);
-  this.grid.endFill();
-}
-}
+    // Make sure we have enough treats on screen
+    for (;this.treats.length < this.expectedTreatCount;) {
+      this.treats.push(this.newTreat())
+    }
+    // Draw the treats
+    for (let i = 0; i < this.treats.length; i++) {
+      let treat = this.treats[i];
+      treat.draw(this.grid);
+    }
+  }
 
   createGrid() {
     let graphics = this.game.add.graphics(0, 0);
     let style = { font: '8px Arial', fill: '#ff0044', wordWrap: true,
-    wordWrapWidth: this.cellSize, align: 'center', backgroundColor: '#ffff00' };
+      wordWrapWidth: this.cellSize, align: 'center', backgroundColor: '#ffff00' };
     // set a fill and line style
     graphics.beginFill(0xFF3300);
     graphics.lineStyle(10, 0xffd900, 1);
