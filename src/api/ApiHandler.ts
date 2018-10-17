d4212754524b5d25f5419a430a9129c90af870a3
Alexander Alemayhu <alexander@alemayhu.com>
2018-10-17 21:03:26 +0200

    Fix issues in the ApiHandler

--- src/api/ApiHandler.ts
+++ src/api/ApiHandler.ts
@@ -7,10 +7,10 @@ import { Player } from './Player';
 
 const path = require('path');
 
-let players: Player[] = [];
-let scripts: SnakeApi.Snake [] = new Array();
-
 export class ApiHandler {
+    players: Player[] = [];
+    scripts: SnakeApi.Snake [] = new Array();
+
     constructor() {
         console.log('Handler initialized');
     }
@@ -18,31 +18,32 @@ export class ApiHandler {
     addScripts(users: string[]): string[] {
         // TODO: Load all users in the chat, use default script for users who have not uploaded a script
         //
-        players = users.map((u) => {
+        users.forEach((u) => {
             if (u === 'ccscanf') {
-                return new Player('interesting.snk', u);
+                this.players.push(new Player('interesting.snk', u));
+            } else {
+                this.players.push(new Player('Example.snk', u));
             }
-
-            new Player('Example.snk', u);
         });
 
         return users.map((u) => u);
     }
 
     compileScripts() {
-        players.forEach(element => {
+        this.players.forEach(element => {
+            console.log('compileScripts', element);
             if (element !== undefined) {
                 console.log(element + ' loaded');
                 const src = decode(require(`../Scripts/${element.script}`));
                 const res: string = ts.transpile(src);
                 const script: any = eval(res);
-                scripts.push(script);
+                this.scripts.push(script);
             }
         });
     }
 
     getNextAction(idx: number, views): Action[] {
-        const sc = scripts[idx];
+        const sc = this.scripts[idx];
         const currentAction = sc.Next.call(sc, SnakeApi)(views);
         console.log('Result:', currentAction);
 
