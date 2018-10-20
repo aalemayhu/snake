5c56f60d79f30a0f4997d62909238c2ff6a24a3c
Mobilpadde <mobilpadde@gmail.com>
2018-10-20 15:52:44 +0200

    Fix scraper-test

--- __tests__/scraper.test.ts
+++ __tests__/scraper.test.ts
@@ -2,11 +2,11 @@ import test from 'ava';
 
 import { Scraper } from '../src/scraping/scraper';
 
-const uri = 'https://gist.github.com/Nyasaki/7ead02f517bc23fad8f58abc46fa8dab';
-const raw = '/raw/5baf3b0387e7c59204aab909ab1371752df73af7/nyasaki_de.ts';
+const uri = 'https://gist.github.com/Mobilpadde/c7df1a0b778eb6e06aa7dd6df19f3ee5';
+const rgx = /^([\w+\:\/\.]+)(raw)\/(\w+)\/(smarty-pants)\.(snk)$/g;
 
 test('scrape', async (t) => {
     const res = await Scraper.GetRawUrl(uri);
 
-    t.is(uri + raw, res);
+    t.truthy(rgx.test(res));
 });
